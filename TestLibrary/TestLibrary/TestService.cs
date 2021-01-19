2a692353de183fcc6149007e28e7230a6c06b3dc
Alexander Dimitrov <alexander@codemerx.com>
2021-01-19 13:22:25 +0200

    Improve test case

---
+++ TestLibrary/TestLibrary/TestService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TestLibrary
+{
+    public class TestService
+    {
+        public void TestMethod(int score)
+        {
+        }
+
+        public void TestMethod2()
+        {
+        }
+    }
+}
